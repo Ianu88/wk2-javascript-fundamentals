a0dd741dfd5092a98cde40672aebf3c23571d04e
IanUpton <uptonian22@gmail.com>
2024-08-05 15:42:29 +0100

    first two activities complete

--- variables.js
+++ variables.js
@@ -4,12 +4,45 @@
 
 //console.log(Count);
 
-let name = "Ian"
+//let name = "Ian"
 
-let age = "35"
+//let age = "35"
 
-let favouriteDrink = "coffee";
-console.log(`Hi. My name is ${name}. I'm + ${age} years old, and my favourite drink is ${favouriteDrink}.`);
-age ="28"
-name = "Chris"
-console.log(`Hi. My name is ${name}. I'm + ${age} years old, and my favourite drink is ${favouriteDrink}.`);
+//let favouriteDrink = "coffee";
+//console.log(`Hi. My name is ${name}. I'm + ${age} years old, and my favourite drink is ${favouriteDrink}.`);
+//age ="28"
+//name = "Chris"
+//console.log(`Hi. My name is ${name}. I'm + ${age} years old, and my favourite drink is ${favouriteDrink}.`);
+
+//Activity 1
+console.log("   |  |    ")
+console.log("   |  |    ")
+console.log("   |  |    ")
+console.log("-----------")
+console.log("   |  |    ")
+console.log("   |  |    ")
+console.log("   |  |    ")
+console.log("-----------")
+console.log("   |  |    ")
+console.log("   |  |    ")
+console.log("   |  |    ")
+
+//Activity 2
+let Breakfast = "Biscuits";
+
+let Lunch = "Hot Dogs";
+
+let Dinner = "Pizza";
+console.log(`Hi. My name is Ian. Today, for breakfast I had ${Breakfast}, for Lunch I Had ${Lunch}, and for Dinner I will have ${Dinner}.`);
+
+Breakfast = "Cereal";
+
+Lunch = "Buttermilk Pancakes";
+
+Dinner = "a Chicken and Mushroom slice";
+console.log(`Hi. My name is Ian. Tomorrow, for breakfast I will have ${Breakfast}, for Lunch I will Have ${Lunch}, and for Dinner I will have ${Dinner}.`)
+//Activity 3
+
+
+
+//Activity 4
