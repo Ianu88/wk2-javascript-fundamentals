8792bf49b52c184aebd876e9fbaa5c3be503f1ff
IanUpton <uptonian22@gmail.com>
2024-08-05 15:05:42 +0100

    activities JS

--- variables.js
+++ variables.js
@@ -11,6 +11,5 @@ let age = "35"
 let favouriteDrink = "coffee";
 console.log(`Hi. My name is ${name}. I'm + ${age} years old, and my favourite drink is ${favouriteDrink}.`);
 age ="28"
-
 name = "Chris"
 console.log(`Hi. My name is ${name}. I'm + ${age} years old, and my favourite drink is ${favouriteDrink}.`);
