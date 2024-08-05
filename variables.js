bda4946f58ba2bb9692ab58bf92011062de9f15d
IanUpton <uptonian22@gmail.com>
2024-08-05 15:04:18 +0100

    class ends JS

---
+++ variables.js
@@ -0,0 +1,16 @@
+//let Count = 10;
+
+//Count *= 2;
+
+//console.log(Count);
+
+let name = "Ian"
+
+let age = "35"
+
+let favouriteDrink = "coffee";
+console.log(`Hi. My name is ${name}. I'm + ${age} years old, and my favourite drink is ${favouriteDrink}.`);
+age ="28"
+
+name = "Chris"
+console.log(`Hi. My name is ${name}. I'm + ${age} years old, and my favourite drink is ${favouriteDrink}.`);
