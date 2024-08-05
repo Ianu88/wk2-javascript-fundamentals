990c509545842813ecd816f07fb5810e614f9b2c
IanUpton <uptonian22@gmail.com>
2024-08-06 00:21:36 +0100

    activity complete

--- variables.js
+++ variables.js
@@ -44,5 +44,24 @@ console.log(`Hi. My name is Ian. Tomorrow, for breakfast I will have ${Breakfast
 //Activity 3
 
 
-
 //Activity 4
+let space1 = "X"
+let space2 = "O"
+let space3 = " "
+let space4 = "X"
+let space5 = "X"
+let space6 = " "
+let space7 = "O"
+let space8 = " "
+let space9 = " "
+console.log("   |   |  ")
+console.log(` ${space1} | ${space2} |  ${space3}  `)
+console.log("   |   |  ")
+console.log("-----------")
+console.log("   |   |  ")
+console.log(` ${space4} | ${space5} |  ${space6}  `)
+console.log("   |   |  ")
+console.log("-----------")
+console.log("   |   |  ")
+console.log(` ${space7} | ${space8} |  ${space9}  `)
+console.log("   |   |  ")
