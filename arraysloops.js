c02b06e2ad42ca7ec074ce2e2931ebc013ea8b04
IanUpton <uptonian22@gmail.com>
2024-08-06 15:47:46 +0100

    end of assignment 1 loops

--- arraysloops.js
+++ arraysloops.js
@@ -1,38 +1,38 @@
-// // // // let coffeOrder = [
-// // // //     "Alex - Cortado",
-// // // //     "Ben - Cortado",
-// // // //     "Charlie - Whatever's new"
-// // // // ]
-// // // // console.log(coffeOrder);
-// // // // coffeOrder[1] = "Ann - Hot Chocolate"
-// // // // console.log(coffeOrder);
+// let coffeOrder = [
+//     "Alex - Cortado",
+//     "Ben - Cortado",
+//     "Charlie - Whatever's new"
+// ]
+// console.log(coffeOrder);
+// coffeOrder[1] = "Ann - Hot Chocolate"
+// console.log(coffeOrder);
 
-// // // // coffeOrder.push("Donna - Espresso")
-// // // // console.log(coffeOrder);
+// coffeOrder.push("Donna - Espresso")
+// console.log(coffeOrder);
 
-// // // // coffeOrder.pop()
-// // // // console.log(coffeOrder);
+// coffeOrder.pop()
+// console.log(coffeOrder);
 
-// // // let favSongs =[
-// // //     "Green Day - Boulevard of Broken Dreams",
-// // //     "Two steps Behind",
-// // //     "Nickelback - How you remind me"
-// // // ]
-// // // console.log(favSongs);
+// let favSongs =[
+//     "Green Day - Boulevard of Broken Dreams",
+//     "Two steps Behind",
+//     "Nickelback - How you remind me"
+// ]
+// console.log(favSongs);
 
-// // // favSongs.push("def-leppard - two steps behind", "Blessed Union of Souls - Brother My Brother")
-// // // console.log(favSongs);
+// favSongs.push("def-leppard - two steps behind", "Blessed Union of Souls - Brother My Brother")
+// console.log(favSongs);
 
-// // // for(let i =0; i < favSongs.length; i++){
-// // //     console.log(favSongs[i]);
-// // // }
-// // let multiplesTwo = [];
-// // for(let i =0; i < 20; i++){
-// //     if(i % 2 == 0){
-// //         multiplesTwo.push(i);
-// //     }
-// // }
-// //     console.log (`numbers divisible by two between 0 and 20 are: ${multiplesTwo}. `);
+// for(let i =0; i < favSongs.length; i++){
+//     console.log(favSongs[i]);
+// }
+// let multiplesTwo = [];
+// for(let i =0; i < 20; i++){
+//     if(i % 2 == 0){
+//         multiplesTwo.push(i);
+//     }
+// }
+//     console.log (`numbers divisible by two between 0 and 20 are: ${multiplesTwo}. `);
 
 // let age = 15;
 // while(age < 18){
@@ -40,3 +40,16 @@
 //     age++
 // }
 //     console.log("Coming right up!");
+
+let favFilms =[
+    "Akira",
+    "NO ONE LIVES",
+    "Pokemon the first movie",
+    "Deathwish",
+    "Cult of Chucky"
+]
+favFilms.push("Nightmare before Christmas", "The Lion King")
+console.log(favFilms);
+
+for(let i =0; i < favFilms; i++){
+    console.log(favFilms[i]);}
