7b0bce8a9233d781114abf3e1c1a8c04fe5a4c22
IanUpton <uptonian22@gmail.com>
2024-08-06 14:39:42 +0100

    first loop

--- arraysloops.js
+++ arraysloops.js
@@ -1,6 +1,24 @@
-let coffeOrder = [
-    "Alex - Cortado",
-    "Ben - Cortado",
-    "Charlie - Whatever's new"
+// let coffeOrder = [
+//     "Alex - Cortado",
+//     "Ben - Cortado",
+//     "Charlie - Whatever's new"
+// ]
+// console.log(coffeOrder);
+// coffeOrder[1] = "Ann - Hot Chocolate"
+// console.log(coffeOrder);
+
+// coffeOrder.push("Donna - Espresso")
+// console.log(coffeOrder);
+
+// coffeOrder.pop()
+// console.log(coffeOrder);
+
+let favSongs =[
+    "Green Day - Boulevard of Broken Dreams",
+    "Two steps Behind",
+    "Nickelback - How you remind me"
 ]
-console.log(coffeOrder);
+console.log(favSongs);
+
+favSongs.push("def-leppard - two steps behind", "kim wilde - kids in america")
+console.log(favSongs);
