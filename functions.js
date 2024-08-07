7c3be1f3b752ff3730f16a1fc15aa8998ea234ea
IanUpton <uptonian22@gmail.com>
2024-08-07 13:53:16 +0100

    functions Activities complete upload

--- functions.js
+++ functions.js
@@ -14,18 +14,64 @@
 // }
 // pressGrindBeans ();
 
-const favColor =(color) =>{
-    console.log (`my favourite colour is ${color}`)
-}
-favColor("white");
-favColor("Azure");
-favColor("maroon");
+// const favColor =(color) =>{
+//     console.log (`my favourite colour is ${color}`)
+// }
+// favColor("white");
+// favColor("Azure");
+// favColor("maroon");
 // let coffeeIsGrinding = false;
 
 // const pressGrindBeans = () =>{
-//     if (coffeeIsGrinding =true)
-//     }
+//     if (coffeeIsGrinding =true)}
 //     console.log("Grinding beans for 20 seconds")
-//  { else if coffeeIsGrinding =false
-//  }
+//  { 
+//     else coffeeIsGrinding =false
+// }
 //  console.log("Grinding beans for 20 seconds")
+
+// const addup = (num1,num2) =>{
+//     return num1+num2
+// }
+// addup (2,3)
+
+// console.log (addup(4,5))
+
+// if (addup(10,4) >10){
+//     console.log("bigger than 10")
+// }
+// else{ console.log ("not")
+// }
+
+//Activity 1
+// const factorial = (n)  => {
+// if ((n===0) || (n===1)) {
+//     return 1;
+// }
+// else {
+// return (n * factorial (n-1));
+// }
+// }
+// console.log (factorial(33));
+
+// Activity 2
+// let orderCount = 0;
+// let topping1 = ("Pineapple")
+// const takeorder = (topping1) => {
+//     console.log(`Pizza with ${topping1}`)
+//     orderCount++;
+// }
+// takeorder("Pineapple")
+
+let pin = "1078" 
+let balance = "£121"
+
+
+if(pin === "1078" && balance <= "£120") {
+       (console.log("dispening your money, please retrieve your card."))}
+else if( pin < "1078") {
+       (console.log("incorrect pin"))}
+else if( pin > "1078") {
+       (console.log("incorrect pin"))}
+else if(balance > "£120") {
+       (console.log("Insufficient funds"))}
