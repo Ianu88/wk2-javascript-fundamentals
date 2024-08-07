35de664ae8e69c060069cd01745e98ba7fc43f2f
IanUpton <uptonian22@gmail.com>
2024-08-07 11:05:47 +0100

    functions third upload

--- functions.js
+++ functions.js
@@ -9,7 +9,23 @@
 // else if(age >= "60") {
 //      (console.log("That's £7.50, would you like help finding a seat?"))}
 
-const pressGrindBeans = () =>{
-    console.log ("Grinding for 20 seconds");
+// const pressGrindBeans = () =>{
+//     console.log ("Grinding for 20 seconds");
+// }
+// pressGrindBeans ();
+
+const favColor =(color) =>{
+    console.log (`my favourite colour is ${color}`)
 }
-pressGrindBeans ();
+favColor("white");
+favColor("Azure");
+favColor("maroon");
+// let coffeeIsGrinding = false;
+
+// const pressGrindBeans = () =>{
+//     if (coffeeIsGrinding =true)
+//     }
+//     console.log("Grinding beans for 20 seconds")
+//  { else if coffeeIsGrinding =false
+//  }
+//  console.log("Grinding beans for 20 seconds")
