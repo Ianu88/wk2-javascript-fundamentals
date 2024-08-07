5db4a9b011278a81290cfbde2d818e436798bf72
IanUpton <uptonian22@gmail.com>
2024-08-07 10:28:23 +0100

    functions first upload

---
+++ functions.js
@@ -0,0 +1,10 @@
+let age = "18"
+
+if(age <= "17") {
+     (console.log("No problem, that's £8 please!"))}
+else if(age == "18") {
+    (console.log("adult ticket, that's £10.95"))}
+else if(18 < age && age < 60) {
+         (console.log("adult ticket, that's £10.95"))}
+else if(age >= "60") {
+     (console.log("That's £7.50, would you like help finding a seat?"))}
