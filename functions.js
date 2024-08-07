14f46a3cdfdd709a20e61c40b86c0e7dae2a6003
IanUpton <uptonian22@gmail.com>
2024-08-07 10:40:51 +0100

    functions second upload

--- functions.js
+++ functions.js
@@ -1,10 +1,15 @@
-let age = "18"
+// let age = "18"
 
-if(age <= "17") {
-     (console.log("No problem, that's £8 please!"))}
-else if(age == "18") {
-    (console.log("adult ticket, that's £10.95"))}
-else if(18 < age && age < 60) {
-         (console.log("adult ticket, that's £10.95"))}
-else if(age >= "60") {
-     (console.log("That's £7.50, would you like help finding a seat?"))}
+// if(age <= "17") {
+//      (console.log("No problem, that's £8 please!"))}
+// else if(age == "18") {
+//     (console.log("adult ticket, that's £10.95"))}
+// else if(18 < age && age < 60) {
+//          (console.log("adult ticket, that's £10.95"))}
+// else if(age >= "60") {
+//      (console.log("That's £7.50, would you like help finding a seat?"))}
+
+const pressGrindBeans = () =>{
+    console.log ("Grinding for 20 seconds");
+}
+pressGrindBeans ();
